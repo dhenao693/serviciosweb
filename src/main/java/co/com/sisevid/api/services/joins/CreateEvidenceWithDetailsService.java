4a2def847281e2379d6623123e24f3a7d07f1783
Daniel Henao <dhenao693@gmail.com>
2022-12-14 14:27:29 -0500

    ajustes para crear evidencia con detalles

---
+++ src/main/java/co/com/sisevid/api/services/joins/CreateEvidenceWithDetailsService.java
@@ -0,0 +1,7 @@
+package co.com.sisevid.api.services.joins;
+
+import co.com.sisevid.api.dto.EvidenceWithDetailsDTO;
+
+public interface CreateEvidenceWithDetailsService {
+    public void createEvidenceWithDetails(EvidenceWithDetailsDTO evidenceWithDetailsDTO);
+}
