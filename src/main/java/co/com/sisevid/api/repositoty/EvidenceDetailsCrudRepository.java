110efe1eefad4245985926cf6a1a762a0dea040a
Daniel Henao <dhenao@lulobank.com>
2022-12-27 16:57:05 -0500

    ajustes para repositorio crud

---
+++ src/main/java/co/com/sisevid/api/repositoty/EvidenceDetailsCrudRepository.java
@@ -0,0 +1,7 @@
+package co.com.sisevid.api.repositoty;
+
+import co.com.sisevid.api.entities.Evidence;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EvidenceDetailsCrudRepository extends CrudRepository<Evidence, Long> {
+}
