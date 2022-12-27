190cd53c3b7df5eaabd46869555637adc305ae34
Daniel Henao <dhenao@lulobank.com>
2022-12-27 12:26:24 -0500

    ajustes para seguridad

--- src/main/java/co/com/sisevid/api/configurations/CORSConfig.java
+++ src/main/java/co/com/sisevid/api/configurations/CORSConfig.java
@@ -1,12 +1,30 @@
 package co.com.sisevid.api.configurations;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @Configuration
 public class CORSConfig {
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+
     @Bean
     public WebMvcConfigurer CORSConfigurer() {
         return new WebMvcConfigurer() {
