da23328260c9a3eb5a51a66df589f8943d2f1c0c
Daniel Henao <dhenao@lulobank.com>
2022-12-27 11:43:27 -0500

    ajustes para dependencias de swagger

--- src/main/java/co/com/sisevid/api/ApiApplication.java
+++ src/main/java/co/com/sisevid/api/ApiApplication.java
@@ -1,13 +1,47 @@
 package co.com.sisevid.api;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
+    @Value("${swagger.server.url}")
+    private String serverUrl;
+
+    private static final String BEARER_AUTH = "bearerAuth";
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setDescription("Server URL");
+        server.setUrl("");
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
+                .components(new Components().addSecuritySchemes(BEARER_AUTH,
+                        new SecurityScheme().name(BEARER_AUTH)
+                                .type(Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addServersItem(server)
+                .info(new Info()
+                        .title("SOA API")
+                        .version("1.0")
+                        .description("SOA API")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 }
